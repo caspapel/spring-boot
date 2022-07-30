653fe0f21159482bec232f104e90febb62ce5376
caspapel <caspapel@gmail.com>
2022-07-30 21:29:31 +0200

    Se cambia controller

--- src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -9,7 +9,7 @@ public class IndexController {
 	
 	@GetMapping(value="/index")
 	public String index(Model model) {
-		model.addAttribute("titulo", "titulo desde java con cambios 2"); 
+		model.addAttribute("titulo", "titulo desde java con cambios en develop"); 
 		return "index" ;
 	}
 
